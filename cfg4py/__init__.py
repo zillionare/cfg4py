3f46555ad8862ffe51bea956a586de0f685a92ab
jieyu <code@jieyu.ai>
2020-06-01 17:41:45 +0800

    Bump version: 0.6.0 → 0.6.1

--- cfg4py/__init__.py
+++ cfg4py/__init__.py
@@ -6,7 +6,7 @@ from cfg4py.core import (
 
 __author__ = """Aaron Yang"""
 __email__ = 'code@jieyu.ai'
-__version__ = "__version__ = '0.6.0'"
+__version__ = "__version__ = '0.6.1'"
 
 
 def get_instance():
