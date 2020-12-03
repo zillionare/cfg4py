66e0794cf597bc56befa6b7564a2a33eafb98f16
jieyu <code@jieyu.ai>
2020-12-03 22:13:17 +0800

    🐞fix: pypi release failed
    version-0.9.0_ is an invalid value for version. caused by single quote in version string

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     packages=find_packages(include=["cfg4py", "cfg4py.*"]),
     setup_requires=setup_requirements,
     url="https://github.com/zillionare/cfg4py",
-    version="version='0.9.0'",
+    version="0.9.0",
     zip_safe=False,
     entry_points={"console_scripts": ["cfg4py=cfg4py.command_line:main"]},
 )
